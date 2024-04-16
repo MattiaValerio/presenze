52303a36cbd0daa319024cd09a85666bfcc99faf
Mattia <prodev@proconsulting.it>
2024-04-16 12:52:12 +0200

    edit drizzle config

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,7 +4,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schemas from '$lib/server/schema';
 
 const pool = new pg.Pool({
-	connectionString: 'postgresql://utenteProva:passwordProva@127.0.0.1:5432/ProTrc?schema=public'
+	connectionString: 'postgresql://utenteProva:passwordProva@postgres:5432/ProTrc?schema=public'
 });
 export const db = drizzle(pool, {
 	schema: { ...schemas },
